8ee1f38bd49b32f77bd12e7d1bbe9dca3eeb5825
aleighbrown <brown.annaleigh@gmail.com>
2021-10-25 18:03:54 +0100

    for the cluster

---
+++ submit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Submit to the cluster, give it a unique name
+#$ -S /bin/bash
+
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1.9G,h_rt=20:00:00,tmem=1.9G
+#$ -pe smp 2
+
+# join stdout and stderr output
+#$ -j y
+#$ -R y
+
+# source activate /SAN/vyplab/vyplab_reference_genomes/conda_envs/splicing_env/
+
+
+
+if [ "$2" != "" ]; then
+    RUN_NAME="$1"_"$2"
+else
+    RUN_NAME=$1
+fi
+
+FOLDER=submissions/$(date +"%Y%m%d%H%M")
+
+mkdir -p ${FOLDER}
+cp config/config.yaml ${FOLDER}/${RUN_NAME}_config.yaml
+
+snakemake -s ${WORKFLOW} \
+--jobscript cluster_qsub.sh \
+--cluster-config config/cluster.yaml \
+--cluster-sync "qsub -l tmem={cluster.tmem},h_vmem={cluster.h_vmem},h_rt={cluster.h_rt} -o $FOLDER {cluster.submission_string}" \
+-j 40 \
+--nolock \
+--rerun-incomplete \
+--latency-wait 100 \
