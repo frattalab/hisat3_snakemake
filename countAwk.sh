9e9702033f0ee4d35ddba56bf70309e82407c2c1
aleighbrown <brown.annaleigh@gmail.com>
2021-10-27 13:07:41 +0100

    whops

--- countAwk.sh
+++ countAwk.sh
@@ -3,4 +3,4 @@
 input=$1
 output=$2
 
-awk  -F "\t" '{print $1 "\t" $2 "\t" $2 + 1 "\t" $5}' $input $output
+awk  -F "\t" '{print $1 "\t" $2 "\t" $2 + 1 "\t" $5}' $input > $output
--- rateAwk.sh
+++ rateAwk.sh
@@ -3,4 +3,4 @@
 input=$1
 output=$2
 
-awk  -F "\t" '{if($7+$5 ==0) $4 = 0; else $4 = ($5)/($7+$5)} {print $1 "\t" $2 "\t" $2 + 1 "\t" $4}' $input $output
+awk  -F "\t" '{if($7+$5 ==0) $4 = 0; else $4 = ($5)/($7+$5)} {print $1 "\t" $2 "\t" $2 + 1 "\t" $4}' $input > $output
