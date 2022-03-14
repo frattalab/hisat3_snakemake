0b936e3bdeb6cb90a91255a6a2ea73e19cfdc941
aleighbrown <brown.annaleigh@gmail.com>
2022-03-14 20:36:09 +0000

    Update calculate_splice_form_stability.py

--- scripts/calculate_splice_form_stability.py
+++ scripts/calculate_splice_form_stability.py
@@ -2,94 +2,129 @@ __author__ = '@aleighbrown'
 import os
 import pysam
 import argparse
-import pickle
-from multiprocessing import Pool
-
-def collect_stats(infile,annotated_sjintervals,cryptic_sjintervals):
+from pathlib import Path
+import pyranges
 
 
-    for read in infile:
+def collect_stats(infile,sj_info):
+    spliced = 0
+    t_coverage = 0
+    spliced_coverted = 0
     
-        if read.is_unmapped:
-            next
-        total_reads += 1
+    sjintervals = set([(sj_info[1],sj_info[2])])
+
+    for read in infile.fetch(sj_info[0]):
+        
         split_read = False
         intronic_region = []
         
-        if read.is_reverse:
-            conversions_on = 'A'
+        if read.is_unmapped:
+            next
         else:
-            conversions_on = 'T'
-        
+            
+            if read.is_reverse:
+                conversions_on = 'A'
+            else:
+                conversions_on = 'T'
+            
+            try:
+                if 'N' in read.cigarstring:
+                    split_read = True
+            except:
+                print("somehow I'm here")
+                if 'N' in read.cigarstring:
+                    split_read = True
+                    print('now i did that')
 
-        if 'N' in read.cigarstring:
-            split_read = True
-            spliced_reads += 1
+            #get all the "splice junctions" in a read
+            if split_read:
 
-        #get all the "splice junctions" in a read
-        if split_read:
+                last_read_pos = False
+                index = 0
+                for read_loc, genome_loc in read.get_aligned_pairs():
 
-            last_read_pos = False
-            index = 0
-            for read_loc, genome_loc in read.get_aligned_pairs():
+                    # get_aligned_pairs returns None for the read location when there is a gap(splice)
+                    #so this sets the start of the splice junction to the genomic location if the read loc is none
+                    if read_loc is None and last_read_pos:
+                        start = genome_loc
 
-                # get_aligned_pairs returns None for the read location when there is a gap(splice)
-                #so this sets the start of the splice junction to the genomic location if the read loc is none
-                if read_loc is None and last_read_pos:
-                    start = genome_loc
+                    # this is to deal with cases where the SJ overhang is only 1 bp and its on the first base 
+                    if index == 1 and read_loc is None:
 
-                # this is to deal with cases where the SJ overhang is only 1 bp and its on the first base 
-                if index == 1 and read_loc is None:
+                        start = genome_loc
 
-                    start = genome_loc
+                    elif read_loc and last_read_pos is None:
 
-                elif read_loc and last_read_pos is None:
+                        stop = genome_loc  # we are right exclusive ,so this is correct
 
-                    stop = genome_loc  # we are right exclusive ,so this is correct
+                        intronic_region.append((start, stop))
 
-                    intronic_region.append((start, stop))
+                        del start
+                        del stop
 
-                    del start
-                    del stop
-
-                last_read_pos = read_loc
-                index +=1
-
-            if set(intronic_region).intersection(annotated_sjintervals):
-                spliced_annotated += 1
-                t_coverage_annotated += read.get_reference_sequence().upper().count(conversions_on)
-
-                conversion_tag = read.get_tag("Yf")
-
-                if conversion_tag > 0:
-                    spliced_annotated_coverted += 1
+                    last_read_pos = read_loc
+                    index +=1
 
 
-            elif set(intronic_region).intersection(cryptic_sjintervals):
-                spliced_cryptic += 1
-                t_coverage_cryptic += read.get_reference_sequence().upper().count(conversions_on)
+                if set(intronic_region).intersection(sjintervals):
 
-                conversion_tag = read.get_tag("Yf")
+                    spliced += 1
 
-                if conversion_tag > 0:
-                    spliced_cryptic_coverted += 1
+                    t_coverage += read.get_reference_sequence().upper().count(conversions_on)
+
+                    conversion_tag = read.get_tag("Yf")
+
+                    if conversion_tag > 0:
+                        spliced_coverted += 1
+
+    rows = [spliced,t_coverage,spliced_coverted]
+
+    return rows
+
+def process_bam(infile,regions):
+    rows = []
+    junctions = pyranges.readers.read_bed(regions, as_df=True, nrows=None)
+    
+
+    samfile = pysam.AlignmentFile(infile, "rb")
+    for index, row in junctions.iterrows():
+        print('Processing Junction Named:')
+        print(row['Name'])
+        jnc = [row['Chromosome'], row['Start'],row['End']]
+        conversion_stats = collect_stats(samfile,jnc)
+        junc_conv = conversion_stats + jnc + [row['Name']]
+        rows.append(junc_conv)
+
+    df = pd.DataFrame(rows, columns=["spliced","t_coverage","spliced_coverted","chr","start","end","name"])
+
+    return df
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bam")
     parser.add_argument("-r", "--regions")
+    parser.add_argument("-o", "--outputfolder")
 
 
     args = parser.parse_args()
 
     bampath = args.bam
-    vcfpath = args.vcf
-    cpu = int(args.cpu)
+    bedpath = args.regions
+    outfolder = args.outputfolder
+
+    basenameBam = Path(bampath).stem
+    basenameBed = Path(bedpath).stem
+
+    outputfile = os.path.join(outfolder, basenameBam + "_" + basenameBed + "_" + "spliced_counts.csv")
+    
+    counts = process_bam(bampath,bedpath)
+
+
+    counts.to_csv(outputfile,index = False)
 
 
 
     return 0
-
 
 if __name__ == "__main__":
     main()
