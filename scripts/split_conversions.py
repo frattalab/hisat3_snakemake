37966ab4b846fca32f6e79cc39c2244b44b0efdc
aleighbrown <brown.annaleigh@gmail.com>
2022-05-26 17:52:46 +0100

    Update split_conversions.py
    add output directory

--- scripts/split_conversions.py
+++ scripts/split_conversions.py
@@ -1,7 +1,7 @@
 import os
 import pysam
 import argparse
-
+from pathlib import Path
 
 
 
@@ -9,16 +9,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bam")
     parser.add_argument("-m", "--mimimum",  default=2)
+    parser.add_argument("-o", "--outputdirectory",  default=2)
+
     
     args = parser.parse_args()
 
+    sampleName = Path(args.bam).stem
 
     infile = pysam.AlignmentFile(args.bam, "rb")
 
-    outfileConverted_path = os.path.splitext(args.bam)[0] + ".convertedreads.bam"
+    outfileConverted_path = os.path.join(args.outputdirectory,sampleName + ".convertedreads.bam" )
     outfileConverted = pysam.AlignmentFile(outfileConverted_path, "wb", template=infile)
 
-    outfileUnConverted_path = os.path.splitext(args.bam)[0] + ".UNconvertedreads.bam"
+    outfileUnConverted_path = os.path.join(args.outputdirectory,sampleName + ".UNconvertedreads.bam" )
     outfileUnConverted = pysam.AlignmentFile(outfileUnConverted_path, "wb", template=infile)
 
 
